a36536379d81b8a97435a48609813832bf59acf7
Tomas <verzbalauskastomas@gmail.com>
2024-02-21 11:00:29 +0200

    Remove extra XXL size in  Size section and add blue in Color section

--- src/main/resources/db/MTB dml.sql
+++ src/main/resources/db/MTB dml.sql
@@ -10,3 +10,32 @@ values ('BIKE', 'bike'),
 insert into gender (code)
 values ('MALE'),
        ('FEMALE');
+
+insert into size (code)
+values  ('XXS'),
+        ('XS'),
+        ('S'),
+        ('M'),
+        ('L'),
+        ('XL'),
+        ('XXL');
+
+insert into color (code)
+values  ('BLACK'),
+        ('WHITE'),
+        ('YELLOW'),
+        ('RED'),
+        ('PURPLE'),
+        ('PINK'),
+        ('GREEN'),
+        ('GRAY'),
+        ('BLUE');
+
+insert into brand (code)
+values ('NIKE'),
+       ('PUMA'),
+       ('ADIDAS'),
+       ('DIESEL'),
+       ('DATCH'),
+       ('CROSS'),
+       ('CALVIN_KLEIN');
