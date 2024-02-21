ee5c5807d5cf173b50d0cfe52b0991d23dd56ca5
Tomas Verzba <148130235+AntiQnis@users.noreply.github.com>
2024-02-21 15:06:16 +0200

    Merge pull request #3
    * Remove extra XXL size in  Size section and add blue in Color section
    
    * make changes in MTB dml file
    
    * removed  repeated label XXS
    
    * removed  repeated label XXS and clean brand fix context
    
    * remove not nesesary brands and add what we working with

--- src/main/resources/db/MTB dml.sql
+++ src/main/resources/db/MTB dml.sql
@@ -10,3 +10,26 @@ values ('BIKE', 'bike'),
 insert into gender (code)
 values ('MALE'),
        ('FEMALE');
+
+insert into size (value)
+values  ('XXS'),
+        ('XS'),
+        ('S'),
+        ('M'),
+        ('L'),
+        ('XL'),
+        ('XXL');
+
+insert into color (code, name)
+values  ('BLACK', 'black'),
+        ('WHITE', 'white'),
+        ('YELLOW', 'yellow'),
+        ('RED', 'red'),
+        ('PURPLE', 'purple'),
+        ('PINK', 'pink'),
+        ('GREEN', 'green'),
+        ('GRAY', 'gray'),
+        ('BLUE', 'blue');
+
+insert into brand (name)
+values ('Context ridewear');
